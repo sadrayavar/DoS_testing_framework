5dc9a0c523fa7c103e976dcfd6463651a8e513ae
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-12-07 12:45:17 +0330

    -add: main pages

---
+++ DoS_testing_website/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("home")
+
+
+def bwHome(request):
+    return HttpResponse("bwHome")
+
+
+def cpuHome(request):
+    return HttpResponse("cpuHome")
+
+
+def ramHome(request):
+    return HttpResponse("ramHome")
+
+
+def home(request):
+    return HttpResponse("home")
+
+
+def home(request):
+    return HttpResponse("home")
